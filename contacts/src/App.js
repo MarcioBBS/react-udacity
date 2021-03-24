61836c84fa0edcd86152a2cb9fafc656f4e4f2c9
MacioBBS <marciobbs@gmail.com>
2021-03-24 11:55:31 -0700

    Add contact list

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,35 +1,80 @@
-const ContactList = props => {   
-  const people = props.contacts;
-  return (
-    <ol>
-      {people.map(person => <li key={person.name}> {person.name} </li> )}
-    </ol>
-  );
-}
+import React, {Component} from 'react';
+import ListContacts from './ListContacts';
 
-const App = () => {
-  return (
-    <div className="App">
-      <ContactList contacts ={[
-        { name: 'Marcelo'},
-        { name: 'Fabio'},
-        { name: 'Jaciara'}
-      ]}/>      
-      <ContactList contacts ={[
-        { name: 'Steve'},
-        { name: 'Ken'},
-        { name: 'John'}
-      ]}/>
-       <ContactList contacts ={[
-        { name: 'Leah'},
-        { name: 'Julian'},
-        { name: 'Pam'}
-      ]}/>
-    </div>
-  );
-}
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
+
+ class App extends Component {
+   render() {
+     return (
+       <div>
+         <ListContacts contacts= {contacts} />
+       </div>
+     )
+   }
+ }
+
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <ListContacts contacts= {contacts} />
+//     </div>
+//   )
+// }
 
 export default App;
+
+// const ContactList = props => {   
+//   const people = props.contacts;
+//   return (
+//     <ol>
+//       {people.map(person => <li key={person.name}> {person.name} </li> )}
+//     </ol>
+//   );
+// }
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <ContactList contacts ={[
+//         { name: 'Marcelo'},
+//         { name: 'Fabio'},
+//         { name: 'Jaciara'}
+//       ]}/>      
+//       <ContactList contacts ={[
+//         { name: 'Steve'},
+//         { name: 'Ken'},
+//         { name: 'John'}
+//       ]}/>
+//        <ContactList contacts ={[
+//         { name: 'Leah'},
+//         { name: 'Julian'},
+//         { name: 'Pam'}
+//       ]}/>
+//     </div>
+//   );
+// }
+
+//export default App;
 
 
 // **** USING CLASSES ****
