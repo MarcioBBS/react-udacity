d40d31e5f4cbc3dd9917e074e299f36780fdddb4
MacioBBS <marciobbs@gmail.com>
2021-03-29 16:10:47 -0700

    Change ListContact to Class Component

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 
-// Stateless Functional Component
-function ListContacts(props) {
-    const contacts = props.contacts;
-    return (
-        <ol className= 'contact-list'>
-                {contacts.map(contact => (
-                    <li key= {contact.id} className= 'contact-list-item'>
-                        <div className='contact-avatar' style= {{backgroundImage: `url(${contact.avatarURL})`}}></div>
-                        <div className= 'contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button 
-                            onClick = {() => props.onDeleteContact(contact)}
-                            className= 'contact-remove'>Remove</button>
-                    </li>
-                ))}
-            </ol>
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
 
-    );
+    render() {
+        return (
+            <ol className= 'contact-list'>
+                    {this.props.contacts.map(contact => (
+                        <li key= {contact.id} className= 'contact-list-item'>
+                            <div className='contact-avatar' style= {{backgroundImage: `url(${contact.avatarURL})`}}></div>
+                            <div className= 'contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+                            <button 
+                                onClick = {() => this.props.onDeleteContact(contact)}
+                                className= 'contact-remove'>Remove</button>
+                        </li>
+                    ))}
+                </ol>
+    
+        );
+    }
 }
 
 export default ListContacts; 
