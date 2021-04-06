0a9ba1a2dc348ad9e50b8e204bbd3e37bf96fa95
MacioBBS <marciobbs@gmail.com>
2021-04-06 11:57:28 -0700

    Create component CreateContact

--- contacts/src/CreateContact.js
+++ contacts/src/CreateContact.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
 import ImageInput from './ImageInput';
 import { Link } from 'react-router-dom';
+import serializeForm from 'form-serialize';
 
 class CreateContact extends Component {
+
+    handleSubmit = event => {
+        event.preventDefault(); // Prevent the browser to serialize the form and throw it in the URL
+
+        // Serialize the form manually and save it into a object        
+        const values = serializeForm(event.target, {hash: true}); // have: true - To get back the object 
+        
+        this.props.onCreateContact && this.props.onCreateContact(values);
+
+    }
     render() {
         return(
             <div>
                 {/* Button Link to home */}
                 <Link to= '/' className='close-create-contact'>Close</Link>
 
-                <form className= 'create-contact-form'>
+                <form onSubmit= {this.handleSubmit} className= 'create-contact-form'>
                     <ImageInput 
                         className= 'create-contact-avatar-input'
                         name= 'avatarURL'
