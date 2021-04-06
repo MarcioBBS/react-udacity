eda087b595a96e34df2ea698d499c54b22ee752f
MacioBBS <marciobbs@gmail.com>
2021-04-06 11:11:44 -0700

    Create list contact template

--- contacts/src/CreateContact.js
+++ contacts/src/CreateContact.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import ImageInput from './ImageInput';
+import { Link } from 'react-router-dom';
 
 class CreateContact extends Component {
     render() {
         return(
             <div>
-                Create Contact
+                {/* Button Link to home */}
+                <Link to= '/' className='close-create-contact'>Close</Link>
+
+                <form className= 'create-contact-form'>
+                    <ImageInput 
+                        className= 'create-contact-avatar-input'
+                        name= 'avatarURL'
+                        maxHeight= {64}
+                    />
+                    <div className= 'create-contact-details'>
+                        <input type= 'text' name= 'name' placeholder= 'Name' />
+                        <input type= 'text' name= 'handle' placeholder= 'Handle' />
+                        <button>Add Contact</button>
+                    </div>
+                </form>
             </div>
         )
     }
